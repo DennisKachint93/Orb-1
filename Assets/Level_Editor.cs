55b557f345c942327db339a1db3224022f129669
Malcolm Balch <malcolmbalch@malcolm-balchs-macbook-pro.local>
2012-11-06 13:50:28 -0600

    began level editor

---
+++ Assets/Level_Editor.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class Level_Editor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Camera.main.orthographicSize = 500;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
