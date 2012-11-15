1f53200bb8ff2c95a824ae976c014c3877c63583
Malcolm Balch <malcolmbalch@malcolm-balchs-macbook-pro.local>
2012-11-15 13:06:04 -0600

    added a space bomb button to outfitter

--- Assets/Outfitter.cs
+++ Assets/Outfitter.cs
@@ -12,7 +12,7 @@ public class Outfitter : MonoBehaviour {
 	
 	private int SHIELD_PRICE = 8;
 	private int ALIEN_GUN_PRICE = 12;
-	private int SPACE_BOMB_PRICE = 16;
+	private int SPACE_BOMB_PRICE = 1;
 	
 	private int BOOST_PRICE = 10;
 	private int SPACE_JUMP_PRICE = 10;
@@ -39,7 +39,8 @@ public class Outfitter : MonoBehaviour {
 	void OnGUI() {	
 		GUI.Label(new Rect(10,10,200,25), "Space Coins: "+gscpt.num_coins);
 		//tier 1 buttons
-		if(GUI.Button(new Rect(10, 45, 200, 25), "Beastly Bending ("+BENDING_PRICE+" coins)")){
+		GUI.Label (new Rect(10,25,200,25), "Tier 1");
+		if(GUI.Button(new Rect(10, 45, 200, 25), "Super Bending ("+BENDING_PRICE+" coins)")){
 			if(gscpt.num_coins >= BENDING_PRICE){
 				//create a new gameobject to replace the old one
 				GameObject pwrup = new GameObject();
@@ -50,7 +51,7 @@ public class Outfitter : MonoBehaviour {
 				gscpt.num_coins -= BENDING_PRICE;
 			}
 		}
-		if(GUI.Button (new Rect(10,75,200,25), "Super Speed ("+SPEED_PRICE+" coins)")) {
+		if(GUI.Button (new Rect(10,75,200,25), "Extra Speed ("+SPEED_PRICE+" coins)")) {
 			if(gscpt.num_coins >= SPEED_PRICE){
 				//create a new gameobject to replace the old one
 				GameObject pwrup = new GameObject();
@@ -72,6 +73,20 @@ public class Outfitter : MonoBehaviour {
 				gscpt.num_coins -= EASY_ENTRY_PRICE;
 			}
 		}
+		
+		GUI.Label (new Rect(10,145,200,25), "Tier 2");
+		if(GUI.Button (new Rect(10,170,200,25), "Space Bomb ("+SPACE_BOMB_PRICE+" coins)")) {
+			if(gscpt.num_coins >= SPACE_BOMB_PRICE){
+				//create a new gameobject to replace the old one
+				GameObject pwrup = new GameObject();
+				//add the script that defines your power up 
+				pwrup.AddComponent("Easy_Entry");
+				//assign the gameobject to the proper tier in game_state
+				gscpt.tier_1_upgrade = pwrup;
+				gscpt.num_coins -= SPACE_BOMB_PRICE;
+			}
+		}
+		
 		
 		//load next level button
 		if(GUI.Button (new Rect(10,Screen.height-30,200,25), "Play next level")) {
