c069cb060774431b79f94fc12906045b984b9494
Malcolm Balch <malcolmbalch@malcolm-balchs-macbook-pro.local>
2012-11-28 11:39:20 -0600

    boost ammo

--- Assets/Powerups/boost.cs
+++ Assets/Powerups/boost.cs
@@ -3,24 +3,25 @@ using System.Collections;
 
 public class boost : MonoBehaviour {
 	
-	//you get 3 boosts when you buy boost
-	private int remaining_boosts = 1;
 	
 	//size of boost
 	private int BOOST_SIZE = 50;
+	Game_State gscpt;
 	
 	// Use this for initialization
 	void Start () {
+		GameObject gs = GameObject.Find("game_state");
+		gscpt = gs.GetComponent<Game_State>();
 	}
 	
 	// Update is called once per frame
 	void Update () {		
-		if(Input.GetKeyDown(KeyCode.C) && remaining_boosts > 0){
+		if(Input.GetKeyDown(KeyCode.C) && gscpt.boost_ammo > 0){
 			//one shot major energy boost
 			Manager.energy += BOOST_SIZE;
 			
 			//decrease remaining boosts
-			remaining_boosts--;
+			gscpt.boost_ammo--;
 		}	
 	}
 }
