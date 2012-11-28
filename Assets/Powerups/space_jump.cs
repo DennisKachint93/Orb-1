e28e332dc5275edeeb02dda59461f05bff60d443
Malcolm Balch <malcolmbalch@malcolm-balchs-macbook-pro.local>
2012-11-28 10:13:33 -0600

    moved space jump behavior to space_jump.cs

--- Assets/Powerups/space_jump.cs
+++ Assets/Powerups/space_jump.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class space_jump : MonoBehaviour {
 
+	private float SPACE_DIST = 100f;
+
 	// Use this for initialization
 	void Start () {
-		Manager.SPACE_JUMP = true;	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		//space jump
+		if(Input.GetKeyUp(KeyCode.C)){
+        	Vector3 jump = Learth_Movement.velocity * SPACE_DIST;
+            Manager.l.transform.position = new Vector3(Manager.l.transform.position.x+jump.x, 
+				Manager.l.transform.position.y+jump.y, Manager.l.transform.position.z+jump.z);
+           	Learth_Movement.lastPos.transform.Translate(jump);
+        }
 	}
 }
