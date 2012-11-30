3e3bee732a9938e273d107cdabcb054fddc424ea
Malcolm Balch <malcolmbalch@ip-75-110.wireless.oberlin.edu>
2012-11-29 22:08:36 -0600

    added space jump bug fix

--- Assets/Powerups/space_jump.cs
+++ Assets/Powerups/space_jump.cs
@@ -1,13 +1,18 @@
+
 using UnityEngine;
 using System.Collections;
 
 public class space_jump : MonoBehaviour {
 
-	private float SPACE_DIST = 100f;
+	
+	private float MAX_JUMP = 350;
 	
 	Manager manager;
 	
+	private float dist_count = 50;
+	private float inc_rate = 1;
 	
+	private bool jumped_at_limit = false;
 	
 	Game_State gscpt;
 
@@ -15,26 +20,33 @@ public class space_jump : MonoBehaviour {
 	void Start () {
 		GameObject gs = GameObject.Find("game_state");
 		gscpt = gs.GetComponent<Game_State>();
-		
-		
 	}
 
 	// Update is called once per frame
 	void Update () {
+	
+		
+		//if you hold E down, it increases the distance for the jump 
+		if(Input.GetKey(KeyCode.E) && gscpt.jump_ammo > 0 && !Learth_Movement.isTangent && !Manager.is_being_attacked)
+		{
+			dist_count += inc_rate;
+			
+			//if you hold for max time, just jump
+			if(dist_count >= MAX_JUMP) {
+				//prevents another jump when you then later release E
+				jumped_at_limit = true;
+				DoJump();
+			}
+			
+		}
 
 		//space jump
 		if(Input.GetKeyUp(KeyCode.E) && gscpt.jump_ammo > 0 && !Learth_Movement.isTangent && !Manager.is_being_attacked){
-			
-			//explosion
-			Instantiate(manager.space_jump_effect,Manager.l.transform.position,Manager.l.transform.rotation);
-			
-			Vector3 jump = Learth_Movement.velocity * SPACE_DIST;
-			Manager.l.transform.position = new Vector3(Manager.l.transform.position.x+jump.x, 
-					Manager.l.transform.position.y+jump.y, Manager.l.transform.position.z+jump.z);
-					
-			Learth_Movement.lastPos.transform.Translate(jump);
-			Instantiate(manager.space_jump_effect,Manager.l.transform.position,Manager.l.transform.rotation);
-			gscpt.jump_ammo--;
+			//jump if E key is legitimately down (not down from having jumped at the limit)
+			if(!jumped_at_limit)
+				DoJump ();
+			else
+				jumped_at_limit = false;
 		}
 		
 		if(gscpt.in_game)
@@ -44,6 +56,22 @@ public class space_jump : MonoBehaviour {
 		}
 		
 		
+		
+	}
+	
+	void DoJump()
+	{
+			//explosion
+			Instantiate(manager.space_jump_effect,Manager.l.transform.position,Manager.l.transform.rotation);
+			
+			Vector3 jump = Learth_Movement.velocity * dist_count;
+			Manager.l.transform.position = new Vector3(Manager.l.transform.position.x+jump.x, 
+					Manager.l.transform.position.y+jump.y, Manager.l.transform.position.z+jump.z);
+					
+			Learth_Movement.lastPos.transform.position += jump;
+			Instantiate(manager.space_jump_effect,Manager.l.transform.position,Manager.l.transform.rotation);
+			gscpt.jump_ammo--;
+			dist_count = 50;
 		
 	}
 }
