47d6d99bdb8d6aa30f21cc30a03d7fb0261af6ee
malcolm <mbc@Malcolms-MacBook-Pro.local>
2012-12-07 10:55:36 -0500

    made invinc pickup grow and shrink

--- Assets/pickups/invincibility/invinc_pickup.cs
+++ Assets/pickups/invincibility/invinc_pickup.cs
@@ -6,9 +6,14 @@ public class invinc_pickup : MonoBehaviour {
 	private bool is_activated = false;
 	private float start_time;
 	private float DURATION = 5;
+	private float grow_shrink_time = 1;
+	private float grow_shrink_rate = .25f;
+	private float start_grow_shrink;
+	private bool shrink = false;
 	
 	// Use this for initialization
 	void Start () {
+		start_grow_shrink = Time.time;
 	
 	}
 	
@@ -20,8 +25,20 @@ public class invinc_pickup : MonoBehaviour {
 			if(Time.time - start_time > DURATION) {
 				Manager.IS_INVINCIBLE = false;
 				Destroy(gameObject);
-			}	
+			}
 		}
+		
+		//make powerup expand and contract
+		if(!is_activated && Time.time - start_grow_shrink > grow_shrink_time) {
+			shrink = !shrink;
+			start_grow_shrink = Time.time;
+		}
+		
+		if(!is_activated) {
+			int ctrl = shrink ? -1 : 1;
+			transform.localScale += new Vector3(ctrl*grow_shrink_rate,ctrl*grow_shrink_rate,ctrl*grow_shrink_rate);
+		}
+		
 		
 	}
 	
